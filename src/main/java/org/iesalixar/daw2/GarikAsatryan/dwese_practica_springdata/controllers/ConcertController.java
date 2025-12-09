d11ad458d4d0ff34a394db8062f58dc7022df753
Garik Asatryan <asa7ur@proton.me>
2025-12-09 19:14:04 +0100

    concert controller done

--- src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_practica_springdata/controllers/ConcertController.java
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_practica_springdata/controllers/ConcertController.java
@@ -1,20 +1,27 @@
 package org.iesalixar.daw2.GarikAsatryan.dwese_practica_springdata.controllers;
 
+import jakarta.validation.Valid;
 import org.iesalixar.daw2.GarikAsatryan.dwese_practica_springdata.entities.Concert;
 import org.iesalixar.daw2.GarikAsatryan.dwese_practica_springdata.entities.dto.ConcertDTO;
+import org.iesalixar.daw2.GarikAsatryan.dwese_practica_springdata.repositories.ArtistRepository;
 import org.iesalixar.daw2.GarikAsatryan.dwese_practica_springdata.repositories.ConcertRepository;
+import org.iesalixar.daw2.GarikAsatryan.dwese_practica_springdata.repositories.StageRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/concerts")
@@ -23,6 +30,15 @@ public class ConcertController {
 
     @Autowired
     private ConcertRepository concertRepository;
+
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    @Autowired
+    private StageRepository stageRepository;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping
     public String listConcerts(
@@ -68,7 +84,92 @@ public class ConcertController {
     @GetMapping("/new")
     public String showNewForm(Model model) {
         logger.info("Solicitando formulario para nuevo agente...");
-        model.addAttribute("agent", new Concert());
+        model.addAttribute("concert", new Concert());
+        model.addAttribute("allArtists", artistRepository.findAll());
+        model.addAttribute("allStages", stageRepository.findAll());
         return "concert-form";
+    }
+
+    @GetMapping("/edit")
+    public String showEditForm(@RequestParam("id") Long id,
+                               RedirectAttributes redirectAttributes,
+                               Model model) {
+        logger.info("Mostrando formulario de edición para la sucursal con ID {}", id);
+        Optional<Concert> concertOpt = concertRepository.findById(id);
+
+        if (concertOpt.isEmpty()) {
+            logger.warn("No se encontró el concierto con ID {}", id);
+
+            String message = messageSource.getMessage("msg.concert.flash.not-found", null, LocaleContextHolder.getLocale());
+            redirectAttributes.addFlashAttribute("errorMessage", message);
+            return "redirect:/concerts";
+        }
+
+        model.addAttribute("concert", concertOpt.get());
+        model.addAttribute("allArtists", artistRepository.findAll());
+        model.addAttribute("allStages", stageRepository.findAll());
+        return "concert-form";
+    }
+
+    @PostMapping("/insert")
+    public String insertConcert(@Valid @ModelAttribute("concert") Concert concert,
+                                BindingResult result,
+                                Model model,
+                                RedirectAttributes redirectAttributes) {
+
+        logger.info("Intentando insertar nuevo concierto...");
+
+        if (result.hasErrors()) {
+            logger.warn("Errores de validación en el formulario de concierto.");
+            model.addAttribute("allArtists", artistRepository.findAll());
+            model.addAttribute("allStages", stageRepository.findAll());
+            return "concert-form";
+        }
+
+        concertRepository.save(concert);
+
+        String message = messageSource.getMessage("msg.concert.flash.created", null, LocaleContextHolder.getLocale());
+        redirectAttributes.addFlashAttribute("successMessage", message);
+
+        return "redirect:/concerts";
+    }
+
+    @PostMapping("/update")
+    public String updateConcert(@Valid @ModelAttribute("concert") Concert concert,
+                                BindingResult result,
+                                Model model,
+                                RedirectAttributes redirectAttributes) {
+
+        logger.info("Actualizando sucursal con ID {}", concert.getId());
+
+        if (result.hasErrors()) {
+            logger.warn("Errores de validación al actualizar el concierto.");
+            model.addAttribute("allArtists", artistRepository.findAll());
+            model.addAttribute("allStages", stageRepository.findAll());
+            return "concert-form";
+        }
+
+        concertRepository.save(concert);
+        logger.info("Concierto con ID {} actualizado con éxito.", concert.getId());
+
+        String message = messageSource.getMessage("msg.concert.flash.updated", null, LocaleContextHolder.getLocale());
+        redirectAttributes.addFlashAttribute("successMessage", message);
+
+        return "redirect:/concerts";
+    }
+
+    @PostMapping("/delete")
+    public String deleteConcert(
+            @RequestParam("id") Long id,
+            RedirectAttributes redirectAttributes
+    ) {
+        logger.info("Eliminando concierto con ID {}", id);
+
+        concertRepository.deleteById(id);
+        logger.info("Concierto con ID {} eliminado correctamente", id);
+
+        String message = messageSource.getMessage("msg.concert.flash.deleted", null, LocaleContextHolder.getLocale());
+        redirectAttributes.addFlashAttribute("successMessage", message);
+        return "redirect:/concerts";
     }
 }
